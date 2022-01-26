36498e4b7d43149d6f31b39fda6da3efca1ac5b0
Adam J <17awesomead@gmail.com>
2022-01-25 20:06:47 -0700

    Changed the ground to allow for better placement of buildings

---
+++ Assets/Node.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    private Renderer myRenderer;
+    private Color startColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = gameObject.GetComponent<Renderer>();
+        startColor = myRenderer.material.color;
+    }
+    private void OnMouseEnter()
+    {
+        myRenderer.material.color = Color.gray;
+    }
+    private void OnMouseExit()
+    {
+        myRenderer.material.color = startColor;
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
