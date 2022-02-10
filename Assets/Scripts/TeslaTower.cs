6c9d6b32e9f9202f437b3686ce90dce29831f2dd
code-a-dragon <70030245+code-a-dragon@users.noreply.github.com>
2022-02-10 13:57:46 -0700

    Changed tesla tower attack pattern

--- Assets/Scripts/TeslaTower.cs
+++ Assets/Scripts/TeslaTower.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 
 public class TeslaTower : MonoBehaviour
 {
-    private float shootDelay = 0.35f;
+    public float shootDelay = 0.35f;
     private float thisTime = 0;
-    private float towerRange = 20.0f;
-    private float maxDamage = 150f;
-    private float minDamage = 1f;
+    public float towerRange = 20.0f;
+    public float maxDamage = 150f;
+    public float minDamage = 1f;
+    public ParticleSystem lightningSystem;
+    private ParticleSystem[] systems;
+    private Boolean playing;
+
 
 
     // Update is called once per frame
     // I am programming the towers to periodically
     // send out a massive shock. Kind of like an EMP shockwave.
     // Perhaps the strength should decrease with the inverse cube of the distance
+
+    private void Awake()
+    {
+        systems = gameObject.GetComponentsInChildren<ParticleSystem>();
+    }
+
     void Update()
     {
         thisTime += Time.deltaTime;
@@ -29,13 +39,26 @@ public class TeslaTower : MonoBehaviour
 
     void SendShock()
     {
+        playing = false;
         foreach (GameObject enemy in GetHorde()) {
             float dist = CalcDistance(enemy);
             if (dist < towerRange) {
+                playing = true;
                 Debug.Log("Shooting enemy");
                 float damage = (float)(maxDamage / Math.Pow(dist + 1, 2));
                 damage = Math.Max(minDamage, damage);
                 enemy.GetComponent<Horde>().Damage(damage);
+            }
+        }
+        foreach (ParticleSystem system in systems)
+        {
+            if (playing)
+            {
+                system.Play();
+            }
+            else
+            {
+                system.Stop();
             }
         }
     }
@@ -51,5 +74,10 @@ public class TeslaTower : MonoBehaviour
     private GameObject[] GetHorde()
     {
         return GameObject.FindGameObjectsWithTag("horde");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(gameObject.transform.position, towerRange);
     }
 }
